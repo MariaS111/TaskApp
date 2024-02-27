c6ffaee7857e5ee21f3f90a6e5050b412f266fef
MariaS111 <mariasviridova470@gmail.com>
2024-02-27 10:53:52 +0300

    feature: edit doc-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,7 @@
 
 python /usr/src/taskapi/manage.py makemigrations --noinput
 python /usr/src/taskapi/manage.py migrate --noinput
-python /usr/src/taskapi/manage.py createsuperuser \
-        --noinput \
-        --username $DJANGO_SUPERUSER_USERNAME \
-        --email $DJANGO_SUPERUSER_EMAIL
+python /usr/src/taskapi/manage.py createsuperuser --noinput  --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 python /usr/src/taskapi/manage.py collectstatic --no-input
 python /usr/src/taskapi/manage.py runserver 0.0.0.0:8000
 
